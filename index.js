3e83880ab96a63c7e0bc284cf5f9575ce564bb54
github username <github email address>
2020-03-20 19:11:55 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,31 @@
 // Your code here
+class Polygon {
+    constructor(number) {
+        this.number = number;
+    }
+
+    get countSides() {
+        return this.number.length;
+    }
+
+    get perimeter() {
+        return this.number.reduce((acc, curr) => acc + curr);
+    }
+
+    get isValid() {
+        return this.number[0] === this.number[1] && this.number[1] === this.number[2];
+    }
+
+    get area() {
+        return Math.pow(this.number[0], 2);
+    }
+}
+
+class Triangle extends Polygon {
+
+
+}
+
+class Square extends Polygon {
+
+}
